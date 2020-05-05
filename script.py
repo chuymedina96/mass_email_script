5e71ace21bd6325d19d5956131aa4a0a7bc945e0
Jesus Medina <chuymedina96@gmail.com>
2020-05-05 14:17:28 -0700

    more

--- script.py
+++ script.py
@@ -1,17 +1,16 @@
 import pandas as pd
 import smtplib
 
-SenderAddress = "chuymedina96@gmail.com"
-# Use app password
-password = "password of sender"
+SenderAddress = "Your email"
+password = "your google app secure password"
 
-e = pd.read_excel("Email.xlsx")
-emails = e['Emails'].values
+e = pd.read_excel("real_moveIn_sheet.xlsx")
+emails = e['Email'].values
 server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()
 server.login(SenderAddress, password)
-msg = "Hello this is a email form python"
-subject = "Hello world"
+msg = "Hello this is a test from a python script."
+subject = "Hello from python test"
 body = "Subject: {}\n\n{}".format(subject,msg)
 for email in emails:
     server.sendmail(SenderAddress, email, body)
